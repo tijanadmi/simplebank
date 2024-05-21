840dac68d0f5a6498df757caaac67f00ee267bf4
tijanadmi <tijanadmi@yahoo.com>
2024-05-21 18:50:22 +0200

    make and yml

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -16,30 +16,9 @@ const (
 	DBSource="postgres://postgres:postgres@localhost:5432/simple_bank"
 )
 
-var testQueries *Queries
 
 func TestMain(m *testing.M) {
-	/*config, err := util.LoadConfig("../..")
-	if err != nil {
-		log.Fatal("cannot load config:", err)
-	}
-
-	connPool, err := pgxpool.New(context.Background(), config.DBSource)
-	if err != nil {
-		log.Fatal("cannot connect to db:", err)
-	}
-
-	testStore = NewStore(connPool)
-	os.Exit(m.Run())*/
-
-	/*conn, err :=sql.Open(dbDriver, DBSource)
-	if err !=nil{
-		log.Fatal("cannot connect to db:", err)
-	}
-
-	testQueries = New(conn)
-
-	os.Exit(m.Run())*/
+	
 
 	//conn, err := pgx.Connect(context.Background(), DBSource)
 	conn, err := pgxpool.New(context.Background(), DBSource)
